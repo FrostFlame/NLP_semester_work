965f4bde5d1fb01bc77b67a25bbd3c99b630259d
Amir Kadyrov <amir.kadyrov.97@mail.ru>
2018-05-17 09:05:49 +0300

    created featureunion, implemented posstats

--- second_task/baseline.py
+++ second_task/baseline.py
@@ -3,6 +3,7 @@ import pandas as pd
 from lxml import objectify
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from sklearn.linear_model import LogisticRegression
@@ -16,6 +17,31 @@ from nltk import word_tokenize
 import numpy as np
 
 sw = stopwords.words('russian')
+
+class PosStats(BaseEstimator, TransformerMixin):
+    def fit(self, x, y=None):
+        return self
+
+    def transform(self, posts):
+        features = []
+        for sentence in posts:
+            tokenized = nltk.tokenize.word_tokenize(sentence[0])
+            tokenized = nltk.pos_tag(tokenized)
+            nn, rb, vb, jj = 0, 0, 0, 0
+            for word in tokenized:
+                if 'NN' in word[1]:
+                    nn += 1
+                elif 'RB' in word[1]:
+                    rb += 1
+                elif 'VB' in word[1]:
+                    vb += 1
+                elif 'JJ' in word[1]:
+                    jj += 1
+            features.append([nn, rb, vb, jj])
+        return features
+
+    def get_feature_names(self):
+        return ['nn', 'rb', 'vb', 'jj']
 
 def tokenize(list_of_sentences):
     stemmer = SnowballStemmer("russian")
@@ -48,33 +74,21 @@ def main():
                                    'second_sentence': second_sentence,
                                    'similarity': similarity})
 
-        tfidf = TfidfVectorizer(analyzer='word',
-                                min_df=3,
-                                ngram_range=(1, 2),
-                                stop_words=sw)
+        feat_union = FeatureUnion( transformer_list=[('tfidf', TfidfVectorizer(analyzer='word',
+                                                                               min_df=3,
+                                                                               ngram_range=(1, 2),
+                                                                               stop_words=sw)),
+                                                     ('cv', CountVectorizer(analyzer='word',
+                                                                            ngram_range=(1, 2))),
+                                                     ('pos', PosStats())])
 
         BagOfWords = pd.concat([class_data.first_sentence, class_data.second_sentence], axis=0)
-        tfidf.fit(BagOfWords)
+        feat_union.fit(BagOfWords)
 
-        train_s1_tfidf = tfidf.transform(class_data.first_sentence)
-        train_s2_tfidf = tfidf.transform(class_data.second_sentence)
+        train_s1_matrix = feat_union.transform(class_data.first_sentence)
+        train_s2_matrix = feat_union.transform(class_data.second_sentence)
 
-        X = abs(train_s1_tfidf - train_s2_tfidf)
-        y = class_data.similarity
-
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=57)
-        lr = LogisticRegression()
-        lr.fit(X_train, y_train)
-        predicted = lr.predict(X_test)
-        print(np.mean(predicted == y_test))
-
-        vect = CountVectorizer()
-        vect.fit(BagOfWords)
-
-        train_s1_tfidf = vect.transform(class_data.first_sentence)
-        train_s2_tfidf = vect.transform(class_data.second_sentence)
-
-        X = abs(train_s1_tfidf - train_s2_tfidf)
+        X = abs(train_s1_matrix - train_s2_matrix)
         y = class_data.similarity
 
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=57)
@@ -87,33 +101,5 @@ def main():
         print(predicted.tolist())
         print(y_test.tolist())
 
-        # X = []
-        # for i in range(len(first_sentence)):
-        #     string = " ".join([first_sentence[i], second_sentence[i]])
-        #     X.append(string)
-        #
-        # y = similarity
-        #
-        # feat_union = FeatureUnion(
-        #     transformer_list=[
-        #         ('tfidf', tfidf),
-        #         ('vect', CountVectorizer(analyzer='word',
-        #                                  ngram_range=(1, 2)))
-        #     ])
-        #
-        # text_clf = Pipeline([
-        #     ('union', feat_union),
-        #     ('clf', LogisticRegression()),
-        # ])
-        #
-        # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=57)
-        #
-        # text_clf.fit(X_train, y_train)
-        # predicted = text_clf.predict(X_test)
-        #
-        # print(np.mean(predicted == y_test))
-        # print(classification_report(y_test, predicted))
-        # print(predicted.tolist())
-        # print(y_test)
 if __name__ == '__main__':
     main()
